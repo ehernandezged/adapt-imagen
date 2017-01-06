f08f077db8462a0a37069e8007f7d5221de0feb2
Emmanuel <ehernandez@grupoeducare.com>
2017-01-06 12:40:04 -0600

    commit

---
+++ js/adapt-imagen.js
@@ -0,0 +1,11 @@
+define([
+    "core/js/adapt",
+    "core/js/views/componentView"
+], function(Adapt, ComponentView) {
+
+    var MyPlugin = {
+       // implement your component
+    };
+    return MyPlugin;
+
+});
